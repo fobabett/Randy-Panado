906bd049159a788b11498db31f14a7445fc9e4e3
Te Vallee <astronomemusic@gmail.com>
2015-12-07 14:06:43 -1000

    implemented slideshow logic in homeController

--- src/modules/app/home/homeController.js
+++ src/modules/app/home/homeController.js
@@ -6,4 +6,20 @@ module.exports = /*@ngInject*/
     $scope.toggleModal = function() {
       $scope.modalShown = !$scope.modalShown;
     };
+
+    $scope.slides = [
+      {name: 'img 1', url: "http://placehold.it/600x300"},
+      {name: 'img 2', url: "http://placehold.it/600x300"},
+      {name: 'img 3', url: "http://placehold.it/600x300"}
+    ]
+
+    $scope.currentIndex = 0;
+    $scope.setCurrentSlideIndex = function (index) {
+      $scope.currentIndex = index;
+    }
+    $scope.isCurrentSlideIndex = function (index) {
+      return $scope.currentIndex === index;
+    };
+
+    console.log($scope.slides);
   };
