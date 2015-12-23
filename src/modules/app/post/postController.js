41d9f32833ebbbae2e1bd1b1d27a38bcb35f3403
Te Vallee <astronomemusic@gmail.com>
2015-12-22 15:59:57 -1000

    mobile drowndown nav styled. needs animaion

--- src/modules/app/post/postController.js
+++ src/modules/app/post/postController.js
@@ -1,7 +1,9 @@
 'use strict';
 
 module.exports = /*@ngInject*/
-  function postController($scope, $stateParams, blogService) {
+  function postController($scope, $stateParams, blogService, $state) {
+
+    $scope.activeTab = 'chronicles';
     
     return blogService.getSingleArticle($stateParams.id)
       .success(function(res) {
