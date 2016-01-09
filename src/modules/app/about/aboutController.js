1fbc076e27e12db058aac9036814a575fdeb2bae
Te Vallee <astronomemusic@gmail.com>
2016-01-08 15:47:41 -1000

    fixed scroll offsets for about/credits/equipment

--- src/modules/app/about/aboutController.js
+++ src/modules/app/about/aboutController.js
@@ -5,7 +5,7 @@ module.exports = /*@ngInject*/
     $timeout(function() { 
       var anchor = angular.element(document.getElementById($stateParams.scrollTo));
       console.log(anchor);
-      $document.scrollToElementAnimated(anchor, -5000, 2000);
+      $document.scrollToElementAnimated(anchor, 0, 2000);
     }, 100);
 
     $scope.activeTab = $state.current.name;
