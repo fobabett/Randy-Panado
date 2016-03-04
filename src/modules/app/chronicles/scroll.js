e128668f97f100afe79071125d622aca2ed80058
Te Vallee <astronomemusic@gmail.com>
2016-03-03 15:22:33 -1000

    RP-27 compressed nav on homepage watches boolChangeClassHome to slide down immediately after header is not visible

--- src/modules/app/chronicles/scroll.js
+++ src/modules/app/chronicles/scroll.js
@@ -3,11 +3,17 @@
 module.exports = /*@ngInject*/
   function scroll($window) {
     return function(scope, element, attrs) {
+      var home = document.getElementById('home-page');
       angular.element($window).bind("scroll", function() {
         if (this.pageYOffset >= 500) {
-            scope.boolChangeClass = true;
+          scope.boolChangeClass = true;
         } else {
-            scope.boolChangeClass = false;
+          scope.boolChangeClass = false;
+        }
+        if (this.pageYOffset >= 100) {
+          scope.boolChangeClassHome = true;
+        } else {
+          scope.boolChangeClassHome = false;
         }
         scope.$apply();
       });
