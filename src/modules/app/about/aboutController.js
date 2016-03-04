47cc7b1c391d6fa65e735c676fe856ae491a363f
Te Vallee <astronomemusic@gmail.com>
2016-03-03 16:05:41 -1000

    RP-32 fixed about/credits/equipment scrolling

--- src/modules/app/about/aboutController.js
+++ src/modules/app/about/aboutController.js
@@ -1,13 +1,34 @@
 'use strict';
 
 module.exports = /*@ngInject*/
-  function aboutController($scope, $state, $location, $anchorScroll, $rootScope, $timeout, $document, $stateParams) {
+  function aboutController($scope, $state, $location, $anchorScroll, $rootScope, $timeout, $document, $stateParams, $window) {
+    if($window.innerWidth <= 1020) {
+        $scope.offset = 140;
+      }
+      if($window.innerWidth >= 1021) {
+      $scope.offset = 80;
+    }
+
     if($stateParams.scrollTo !== null) {
       $timeout(function() { 
         var anchor = angular.element(document.getElementById($stateParams.scrollTo));
-        console.log(anchor);
         $document.scrollToElementAnimated(anchor, 0, 2000);
       }, 100);
+    }
+
+    $scope.scrollToSection = function(hash) {
+      if(hash === 'credits') {
+        var anchor = document.getElementById('credits-section');
+      } 
+      else if(hash === 'equipment') {
+        var anchor = document.getElementById('equipment-section');
+      }
+      if($window.innerWidth <= 1020) {
+        $document.scrollToElementAnimated(anchor, 140, 1000);
+      }
+      if($window.innerWidth >= 1021) {
+        $document.scrollToElementAnimated(anchor, 80, 1000);
+      }
     }
 
     $scope.activeTab = $state.current.name;
